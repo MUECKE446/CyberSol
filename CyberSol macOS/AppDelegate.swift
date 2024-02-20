7619d81c38e2431b3957f298567f0bbb736f02ad
Christian Muth <christian-muth@t-online.de>
2024-02-20 09:26:52 +0100

    Initial Commit

---
+++ CyberSol macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  CyberSol macOS
+//
+//  Created by Christian Muth on 20.02.24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
